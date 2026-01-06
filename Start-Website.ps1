61a112c98b11a8fc1bb83355444be7b70c554ae7
Ray Reinman <ray3639@comcast.net>
2026-01-06 11:39:48 -0600

    Initial SolveMotion Labs website

---
+++ Start-Website.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host "=== SolveMotionWebSite Local Server ===" -ForegroundColor Cyan
+Set-Location "C:\src\SolveMotionWebSite"
+Write-Host "Serving from: C:\src\SolveMotionWebSite" -ForegroundColor Yellow
+Write-Host "Starting: py -m http.server 8080" -ForegroundColor Green
+py -m http.server 8080

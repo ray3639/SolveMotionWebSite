3014e8671575bafcd68c803bc19d8f51d7e6a9f4
Ray Reinman <ray3639@comcast.net>
2026-05-10 20:00:58 -0500

    Publish website upgrade and SolvePharmacy web app portal

--- webapps/solvemotion/flutter_bootstrap.js
+++ webapps/solvemotion/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3640362538" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "147265790" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

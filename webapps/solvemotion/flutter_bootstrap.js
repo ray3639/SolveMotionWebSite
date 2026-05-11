683228d9b30eff6ca548b6705d29cea0726222b0
Ray Reinman <ray3639@comcast.net>
2026-05-11 10:45:04 -0500

    Publish SolveMotion web build 143 restore full camera viewport

--- webapps/solvemotion/flutter_bootstrap.js
+++ webapps/solvemotion/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3933044895" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3003794613" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

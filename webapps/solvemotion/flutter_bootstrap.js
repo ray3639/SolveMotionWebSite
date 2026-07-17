b5150444835ade3e3dd1bff5a1c995f71d0e2810
Ray Reinman <ray3639@comcast.net>
2026-07-16 20:30:25 -0500

    Publish SolveMotion 6.0 build 159

--- webapps/solvemotion/flutter_bootstrap.js
+++ webapps/solvemotion/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1770902514" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3853009533" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

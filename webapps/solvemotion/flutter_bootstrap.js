b11051b06178be218834277b6c63e89e2a7767dd
Ray Reinman <ray3639@comcast.net>
2026-07-29 23:22:48 -0500

    Publish SolveMotion 6.0 build 174 Android startup fix

--- webapps/solvemotion/flutter_bootstrap.js
+++ webapps/solvemotion/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2597748675" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1851003568" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

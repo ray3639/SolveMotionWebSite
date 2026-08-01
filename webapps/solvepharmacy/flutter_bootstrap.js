40c42efafcaf7716b26b279ef01f51f3cfa84dbb
Ray Reinman <ray3639@comcast.net>
2026-08-01 18:57:54 -0500

    Publish SolvePharmacy 6.0 build 90

--- webapps/solvepharmacy/flutter_bootstrap.js
+++ webapps/solvepharmacy/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2890329840" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2429501211" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });

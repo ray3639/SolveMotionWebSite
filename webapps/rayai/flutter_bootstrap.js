0482cc179bff4162246c3ed2e4c85d3f0d7ff170
Ray Reinman <ray3639@comcast.net>
2026-09-13 18:28:43 -0500

    Publish RayAI 6.0.0+35 to solvemotionlabs.com

--- webapps/rayai/flutter_bootstrap.js
+++ webapps/rayai/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3315350409" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4027239458" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
